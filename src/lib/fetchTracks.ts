65a4f20ed42481230e6e735e0a056942c282adea
Katalz <57660079+Katalz@users.noreply.github.com>
2023-09-19 16:24:53 +0200

    1 file modified

--- src/lib/fetchTracks.ts
+++ src/lib/fetchTracks.ts
@@ -1,7 +1,7 @@
 import { SavedTrack } from 'spotify-types';
 
 const apiToken =
-  'BQCgWlCiXnKwqbvyHqmINOsCLQhjHma_cgx-tuYIijmkfvuvkmlGwB1PRbpK6oD-qw9B8PxaltLOaaqyiJMqaQi5XvKKWth8h6AtsTbLL5cxqYa70SE9aNnYNayo2mkkDd_s9juU5ZHo-KYnukufQl_71HtRBbshfrs4akBhujtaihmo7__imXZwv6r0jr3LQbyd4ozcZWJXdm0lDNlOk5xjXv_pFg';
+  'BQAoA1sSea4PK5f_Nd2F4hmEVZB6gRRxayO2NisAz1EgZR38MY7xam99kD6-3zF8NLBLo8QkRGWP5OyHJECLNOPNHiaae-UFQnHl_vRNcb6BtnjhOcygKCTT92TdEBP7q_pAyTvTeQgww59Q3mErsNn6p81UzY6nfaqBuD6Jx25jWbLF0J4-I79SKLOo49tPeazfYQ3ZkhVkomSkbiGk51D64HXrbA';
 
 export const fetchTracks = async (): Promise<SavedTrack[]> => {
   const response = await fetch('https://api.spotify.com/v1/me/tracks', {
