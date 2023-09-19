8bbe243815793c9555af6ad67088ae1543484478
Katalz <57660079+Katalz@users.noreply.github.com>
2023-09-19 15:03:43 +0200

    jalon 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import logo from './assets/logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { fetchTracks } from './lib/fetchTracks';
+import { useQuery } from '@tanstack/react-query';
 const trackUrls = [
   'https://p.scdn.co/mp3-preview/742294f35af9390e799dd96c633788410a332e52',
   'https://p.scdn.co/mp3-preview/5a12483aa3b51331aba663131dbac967ccb33d99',
@@ -7,7 +10,16 @@ const trackUrls = [
   'https://p.scdn.co/mp3-preview/0f6b8a3524ec410020457da4cdd7717f9addce2f',
   'https://p.scdn.co/mp3-preview/ac28d1b0be285ed3bfd8e9fa5fad133776d7cf36',
 ];
+
 const App = () => {
+  const { data: tracks } = useQuery({
+    queryKey: ['tracks'],
+    queryFn: fetchTracks,
+  });
+  const [trackIndex, setTrackIndex] = useState(0);
+  const goToNextTrack = () => {
+    setTrackIndex(trackIndex + 1);
+  };
   return (
     <div className="App">
       <header className="App-header">
@@ -15,7 +27,8 @@ const App = () => {
         <h1 className="App-title">Bienvenue sur le blind test</h1>
       </header>
       <div className="App-images">
-        <audio src={trackUrls[0]} autoPlay controls />
+        <audio src={trackUrls[trackIndex]} autoPlay controls />
+        <button onClick={goToNextTrack}>Next track</button>
         <p>Il va falloir modifier le code pour faire un vrai blind test !</p>
       </div>
       <div className="App-buttons"></div>
--- src/lib/fetchTracks.ts
+++ src/lib/fetchTracks.ts
@@ -1,5 +1,17 @@
-const apiToken: string = '';
+const apiToken =
+  'BQA4g9Fii_v_u1TOWXWl120j-7lTzJIvJVDB6gBlBy5fJt-grIV3GwzCcRpxY0O7p0oDzfRWeWquvQ0Ym9mVs28UuF6ajKK4GjmeH5AGybhlSIBDr_Bee-bfrsBHVxU8ndQy8ZCVwL20TGk7DJu1Kujrp2Ua7PFcXQsAotRNqP-Tavnh2yDVJhrabAdkd99vlrY-nd6IFckt8VsAQwe96VZMUxS9Kw';
 
 export const fetchTracks = async () => {
-  // This function does nothing, yet
+  const response = await fetch('https://api.spotify.com/v1/me/tracks', {
+    method: 'GET',
+    headers: {
+      Authorization: 'Bearer ' + apiToken,
+    },
+  });
+  if (!response.ok) {
+    throw new Error(`Fetching tracks failed with status ${response.status}`);
+  }
+  const data = (await response.json()) as { items: unknown[] };
+
+  return data.items;
 };
